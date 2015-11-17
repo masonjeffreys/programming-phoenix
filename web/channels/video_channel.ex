e07b05315dae729a355d740ed73a3f4b86eee557
Jeff Mason <masonjeffreys@gmail.com>
2015-11-16 20:46:06 -0500

    WorkingApp

--- web/channels/video_channel.ex
+++ web/channels/video_channel.ex
@@ -1,22 +1,43 @@
 defmodule Rumbl.VideoChannel do
 	use Rumbl.Web, :channel
 
+	alias Rumbl.AnnotationView
+
+	def join("videos:" <> video_id, _params, socket) do
+		video = Repo.get!(Rumbl.Video, video_id)
+		annotations = Repo.all(
+			from a in assoc(video, :annotations),
+			order_by: [desc: a.at],
+			limit: 200,
+			preload: [:user]
+		)
+		resp = %{annotations: Phoenix.View.render_many(annotations, AnnotationView, "annotation.json")}
+		{:ok, resp, assign(socket, :video_id, video_id)}
+	end
+
 	def handle_info(:ping, socket) do
 		count = socket.assigns[:count] || 1
 		push socket, "ping", %{count: count}
 		{:noreply, assign(socket, :count, count + 1)}
 	end
 
-	def join("videos:" <> video_id, _params, socket) do
-		{:ok, assign(socket, :video_id, video_id)}
+  	def handle_in("new_annotation", params, socket) do
+		user = socket.assigns.current_user
+		changeset =
+			user
+			|> build(:annotations, video_id: String.to_integer(socket.assigns.video_id))
+			|> Rumbl.Annotation.changeset(params)
+		case Repo.insert(changeset) do
+			{:ok, annotation} ->
+				broadcast! socket, "new_annotation", %{
+					user: Rumbl.UserView.render("user.json", %{user: user}),
+					body: annotation.body,
+					at: annotation.at
+				}
+			{:reply, :ok, socket}
+			{:error, changeset} ->
+				{:reply, {:error, %{errors: changeset}}, socket}
+		end
 	end
 
-	def handle_in("new_annotation", params, socket) do
-		broadcast! socket, "new_annotation", %{
-			user: %{username: "anon"},
-			body: params["body"],
-			at: params["at"]
-		}
-		{:reply, :ok, socket}
-	end
 end
